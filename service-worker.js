f0d039b7d03e7a4c34922a722151f3a8fa08ff99
Fossio-FTG-MCK <fossioftg@gmail.com>
2025-05-23 19:15:57 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,29 @@
 const CACHE_NAME = 'v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/layout.html',
-  '/login-signup-reset.html',
-  '/pages/offline.html',
-  '/pages/ajuda.html',
-  '/pages/compras.html',
-  '/pages/extrato.html',
-  '/pages/contato.html',
-  '/pages/meus-dados.html',
-  '/pages/parceiros.html',
-  '/pages/vouchers.html',
-  '/pages/dashboard.html',
-  '/pages/ajuda.css',
-  '/pages/compras.css',
-  '/pages/extrato.css',
-  '/pages/contato.css',
-  '/pages/meus-dados.css',
-  '/pages/parceiros.css',
-  '/pages/vouchers.css',
-  '/pages/dashboard.css',
-  '/app.js',
-  '/midias/logo.png', // Adicione o caminho correto para a logo
+  './',
+  './index.html',
+  './style.css',
+  './layout.html',
+  './login-signup-reset.html',
+  './pages/offline.html',
+  './pages/ajuda.html',
+  './pages/compras.html',
+  './pages/extrato.html',
+  './pages/contato.html',
+  './pages/meus-dados.html',
+  './pages/parceiros.html',
+  './pages/vouchers.html',
+  './pages/dashboard.html',
+  './pages/ajuda.css',
+  './pages/compras.css',
+  './pages/extrato.css',
+  './pages/contato.css',
+  './pages/meus-dados.css',
+  './pages/parceiros.css',
+  './pages/vouchers.css',
+  './pages/dashboard.css',
+  './app.js',
+  './midias/logo.png', // Adicione o caminho correto para a logo
   // Adicione outros arquivos de mídia conforme necessário
 ];
 
@@ -45,7 +45,7 @@ self.addEventListener('fetch', event => {
           return response;
         }
         return fetch(event.request).catch(() => {
-          return caches.match('/pages/offline.html');
+          return caches.match('./pages/offline.html');
         });
       })
   );
